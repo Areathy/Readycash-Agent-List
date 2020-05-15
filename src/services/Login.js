18ad081aa95fe178c6fea5318461a70295562e16
jilesanmi <ilesanmi.josiah@gmail.com>
2020-05-15 16:49:15 +0100

    login

--- src/services/Login.js
+++ src/services/Login.js
@@ -18,9 +18,8 @@ export default {
   methods: {
     fetchUsers() {
       const params = new URLSearchParams();
-      params.append('hashed password', hashPassword(this.password))
       params.append('userName', this.userName); // 0000100047
-      // params.append('password', this.password); // 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
+       params.append('password', hashPassword(this.password)); // 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
       axios.post('http://62.173.32.30:8080/rc/rest/agent/login', params)
       .then(res=>{
         console.log(res);
