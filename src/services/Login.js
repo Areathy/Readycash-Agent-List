2aed10f30bcdbd4c22015789439a29f97a831f46
areathy <areathy33@gmail.com>
2020-05-15 16:36:14 +0100

    testing password

--- src/services/Login.js
+++ src/services/Login.js
@@ -1,8 +1,8 @@
-import axios from "axios";
-import {hashPassword} from "./hash.js";
+import axios from 'axios';
+import {hashPassword} from './hash.js';
 
 export default {
-  name: "Login",
+  name: 'Login',
 
   components: {},
 
@@ -18,11 +18,12 @@ export default {
   methods: {
     fetchUsers() {
       const params = new URLSearchParams();
-      console.log("hashed password",hashPassword(this.password))
+      params.append('hashed password', hashPassword(this.password))
       params.append('userName', this.userName); // 0000100047
-      params.append('password', this.password); // 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
+      // params.append('password', this.password); // 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
       axios.post('http://62.173.32.30:8080/rc/rest/agent/login', params)
       .then(res=>{
+        console.log(res);
         if(res) {
           const auth = res.headers.authorization
           localStorage.setItem('authorization', auth)
@@ -34,7 +35,7 @@ export default {
 
       .catch(e=>{
         if(e) {
-          this.errors = "Incorrect username or password" 
+          this.errors = 'Incorrect password entered, Please check your password and try again' 
           alert(this.errors)
         }
       })
