ed822f8e6edb6c98b9b5157e2babd89a4b94a2be
areathy <areathy33@gmail.com>
2020-05-22 18:40:42 +0100

    Added more condition to clearInterval

--- src/components/agent-item/script.js
+++ src/components/agent-item/script.js
@@ -56,7 +56,7 @@ export default {
         this.balanceState.loading = false;
       })
       .catch( (error) => {
-        if(error) {          
+        if(error && !auth) {          
           clearInterval(this.refreshBalance);
         }
       }); 
@@ -72,7 +72,7 @@ export default {
       if (!auth) {
         this.$router.push('/');
         this.$noty.error('Bearer token is not set', {
-          timeout: 5000,
+          timeout: 10000,
           layout: 'topCenter',
           theme: 'metroui',
         })
