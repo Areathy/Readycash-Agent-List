ef52ec3a2ec6e5714a7fcf1fdf2755737270fd5b
jilesanmi <ilesanmi.josiah@gmail.com>
2020-06-10 12:45:53 +0100

    rf

--- src/components/agent/Agent.js
+++ src/components/agent/Agent.js
@@ -1,82 +1,55 @@
-import api from '../../apis/readycash';
-import AgentItem from "../agent-item/index.vue";
+import api from "../../apis/readycash";
+import AgentItem from "../agent-balance/index.vue";
 
 export default {
   name: "Agent",
 
-  components: {AgentItem},
+  components: { AgentItem },
 
-  data: function () {
+  data: function() {
     return {
       agents: [],
       balance: null,
       errors: null,
-      search:"",
-      isSearch: false
-    }
+      search: "",
+    };
   },
-  
+
   mounted() {
     this.fetchUsers();
-    this.redirectUser();
-    this.reLogin();
   },
 
   computed: {
-      filteredAgents() {
-        return this.agents.filter( agent => {
-          return agent.fullName.toLowerCase().includes(this.search.toLowerCase()) || 
-                agent.phoneNumber.toLowerCase().includes(this.search.toLowerCase()) ||
-                agent.realId.toLowerCase().includes(this.search.toLowerCase())
-       }) 
-      }
+    filteredAgents() {
+      return this.agents.filter((agent) => {
+        return (
+          agent.fullName.toLowerCase().includes(this.search.toLowerCase()) ||
+          agent.phoneNumber.toLowerCase().includes(this.search.toLowerCase()) ||
+          agent.realId.toLowerCase().includes(this.search.toLowerCase())
+        );
+      });
+    },
   },
 
   methods: {
     fetchUsers() {
-      const auth = localStorage.getItem('authorization')
-      api.get('/agent/downlines', {
-        headers: {
-          'Authorization': auth
-        }
-      })
-        .then(res => {
-          this.agents = res.data
-          console.log(this.agents)
+      const auth = localStorage.getItem("authorization");
+      api
+        .get("/agent/downlines", {
+          headers: {
+            Authorization: auth,
+          },
         })
-        
-        .catch( (error) => {
-          if(error) {
-            console.log(error.response.data);
-            this.errors = this.$noty.error(error.response.data, {
-              timeout: 5000,
-              layout: 'topCenter',
-              theme: "metroui",
-            })
+        .then((res) => {
+          this.agents = res.data;
+        })
+
+        .catch((error) => {
+          if (error) {
+            localStorage.clear();
+            this.$router.push("/");
           }
-      })    
-    }, 
-
-    redirectUser() {
-      const auth = localStorage.getItem('authorization')
-      if (!auth) {
-        this.$router.push('/');
-      }
+        });
     },
-    
-    reLogin() {
-      setTimeout(() => {
-        localStorage.clear();
-      }, 5400000);
-   }
-  }
-
-  // computed: {
-  //   agentSearch: function() {
-  //     return this.agents.filter((agent) =>{
-  //       return agent.fullName.match(this.search)
-  //     })
-  //   }
-  // }
-
-}
+  },
+};
